80ed6ee7abe51b89184fa371512e342bbf8fdb6f
Jinzhou Zhang <lotabout@gmail.com>
2018-04-08 15:59:20 +0800

    [arg] parse pattern and commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#[macro_use] extern crate structopt;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "Rargs", about = "Xargs with pattern matching")]
+#[structopt(raw(setting = "structopt::clap::AppSettings::TrailingVarArg"))]
+struct Rargs {
+    #[structopt()]
+    pattern: String,
+
+    #[structopt()]
+    command: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let exit_code = real_main();
+    std::process::exit(exit_code);
+}
+
+fn real_main() -> i32 {
+    let opts = Rargs::from_args();
+    println!("{:?}", opts);
+    1
 }
