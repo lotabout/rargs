28995bf295477ca7fcdfd7f70068c8cf99664df4
Jinzhou Zhang <lotabout@gmail.com>
2018-04-09 18:33:42 +0800

    [tests] modify tests to work with CI environment

--- tests/test.py
+++ tests/test.py
@@ -29,142 +29,134 @@ class TestRargs(unittest.TestCase):
         cmd = f"{input} | {RARGS} {args} {command}"
         return self._execute(cmd)
 
-    def _echo(self):
-        return 'gecho' if sys.platform == 'darwin' else 'echo'
-
-    def test_echo(self):
-        # echo -e 'a\\nb\nc\\nd' | rargs pattern echo -e {}
-        # => a\nb\nc\nd\n
-        output, _ = self._rargs(input=r"echo -e 'a\\nb\nc\\nd'",
-                                command='{} -e {{}}'.format(self._echo()))
-        self.assertEqual(output, 'a\nb\nc\nd\n')
-
     def test_regex_should_match(self):
-        # echo '2018-01-20' | rargs '^(?P<year>\d{4})-(\d{2})-(\d{2})$' echo -e {1} {2} {3}
+        # echo '2018-01-20' | rargs '^(?P<year>\d{4})-(\d{2})-(\d{2})$' echo -n {1} {2} {3}
         # => a\nb\nc\nd\n
         output, _ = self._rargs(input=r"echo '2018-01-20'",
                                 args="""-p '^(?P<year>\d{4})-(\d{2})-(\d{2})$'""",
-                                command='{} -e {{1}} {{2}} {{3}}'.format(self._echo()))
-        self.assertEqual(output, '2018 01 20\n')
+                                command='echo -n {1} {2} {3}')
+        self.assertEqual(output, '2018 01 20')
 
     def test_regex_group_name_should_match(self):
-        # echo '2018-01-20' | rargs '^(?P<year>\d{4})-(\d{2})-(\d{2})$' echo -e {year} {2} {3}
+        # echo '2018-01-20' | rargs '^(?P<year>\d{4})-(\d{2})-(\d{2})$' echo -n {year} {2} {3}
         # => a\nb\nc\nd\n
         output, _ = self._rargs(input=r"echo '2018-01-20'",
                                 args="""-p '^(?P<year>\d{4})-(\d{2})-(\d{2})$'""",
-                                command='{} -e {{year}} {{2}} {{3}}'.format(self._echo()))
-        self.assertEqual(output, '2018 01 20\n')
+                                command='echo -n {year} {2} {3}')
+        self.assertEqual(output, '2018 01 20')
 
     def test_negtive_regex_group_should_work(self):
-        # echo '2018-01-20' | rargs '^(?P<year>\d{4})-(\d{2})-(\d{2})$' echo -e {-3} {-2} {-1}
+        # echo '2018-01-20' | rargs '^(?P<year>\d{4})-(\d{2})-(\d{2})$' echo -n {-3} {-2} {-1}
         # => a\nb\nc\nd\n
         output, _ = self._rargs(input=r"echo '2018-01-20'",
                                 args=r" -p '^(?P<year>\d{4})-(\d{2})-(\d{2})$'",
-                                command='{} -e {{-3}} {{-2}} {{-1}}'.format(self._echo()))
-        self.assertEqual(output, '2018 01 20\n')
+                                command='echo -n {-3} {-2} {-1}')
+        self.assertEqual(output, '2018 01 20')
 
     def test_read0_short(self):
-        output, _ = self._rargs(input=r"echo -e 'a\0b'",
+        find_output, _ = self._execute('find . -d 1')
+        output, _ = self._rargs(input="find . -d 1 -print0",
                                 args='-0',
-                                command='{} -e X{{}}X'.format(self._echo()))
-        self.assertEqual(output, 'XaX\nXbX\n')
+                                command='echo {}')
+        self.assertEqual(output, find_output)
 
     def test_read0_long(self):
-        output, _ = self._rargs(input=r"echo -e 'a\0b'",
+        find_output, _ = self._execute('find . -d 1')
+        output, _ = self._rargs(input="find . -d 1 -print0",
                                 args='--read0',
-                                command='{} -e X{{}}X'.format(self._echo()))
-        self.assertEqual(output, 'XaX\nXbX\n')
+                                command='echo {}')
+        self.assertEqual(output, find_output)
 
     def test_no_read0(self):
-        _, err = self._rargs(input=r"echo -e 'a\0b'",
-                             command='{} -e X{{}}X'.format(self._echo()))
+        _, err = self._rargs(input=r"echo -n 'a\0b'",
+                             command='echo -n X{}X')
         self.assertNotEqual(err, None)
 
     def test_default_delimiter(self):
-        output, _ = self._rargs(input=r"echo -e 'a b  c'",
-                                command='{} -e X{{1}},{{2}},{{3}}X'.format(self._echo()))
-        self.assertEqual(output, "Xa,b,cX\n")
+        output, _ = self._rargs(input=r"echo -n 'a b  c'",
+                                command='echo -n X{1},{2},{3}X')
+        self.assertEqual(output, "Xa,b,cX")
 
     def test_delimiter(self):
-        output, _ = self._rargs(input=r"echo -e 'a,b,,c'",
+        output, _ = self._rargs(input=r"echo -n 'a,b,,c'",
                                 args='-d ,',
-                                command='{} -e X{{1}},{{2}},{{3}},{{4}}X'.format(self._echo()))
-        self.assertEqual(output, "Xa,b,,cX\n")
+                                command='echo -n X{1},{2},{3},{4}X')
+        self.assertEqual(output, "Xa,b,,cX")
 
     def test_range_left_inf(self):
-        output, _ = self._rargs(input=r"echo -e '1,2,3,4,5,6'",
+        output, _ = self._rargs(input=r"echo -n '1,2,3,4,5,6'",
                                 args='-d ,',
-                                command='{} -e X{{..3}}X'.format(self._echo()))
-        self.assertEqual(output, "X1 2 3X\n")
+                                command='echo -n X{..3}X')
+        self.assertEqual(output, "X1 2 3X")
 
-        output, _ = self._rargs(input=r"echo -e '1,2,3,4,5,6'",
+        output, _ = self._rargs(input=r"echo -n '1,2,3,4,5,6'",
                                 args='-d ,',
-                                command='{} -e X{{..-2}}X'.format(self._echo()))
-        self.assertEqual(output, "X1 2 3 4 5X\n")
+                                command='echo -n X{..-2}X')
+        self.assertEqual(output, "X1 2 3 4 5X")
 
-        output, _ = self._rargs(input=r"echo -e '1,2,3,4,5,6'",
+        output, _ = self._rargs(input=r"echo -n '1,2,3,4,5,6'",
                                 args='-d ,',
-                                command='{} -e X{{..0}}X'.format(self._echo()))
-        self.assertEqual(output, "XX\n")
+                                command='echo -n X{..0}X')
+        self.assertEqual(output, "XX")
 
     def test_range_right_inf(self):
-        output, _ = self._rargs(input=r"echo -e '1,2,3,4,5,6'",
+        output, _ = self._rargs(input=r"echo -n '1,2,3,4,5,6'",
                                 args='-d ,',
-                                command='{} -e X{{3..}}X'.format(self._echo()))
-        self.assertEqual(output, "X3 4 5 6X\n")
+                                command='echo -n X{3..}X')
+        self.assertEqual(output, "X3 4 5 6X")
 
-        output, _ = self._rargs(input=r"echo -e '1,2,3,4,5,6'",
+        output, _ = self._rargs(input=r"echo -n '1,2,3,4,5,6'",
                                 args='-d ,',
-                                command='{} -e X{{-2..}}X'.format(self._echo()))
-        self.assertEqual(output, "X5 6X\n")
+                                command='echo -n X{-2..}X')
+        self.assertEqual(output, "X5 6X")
 
-        output, _ = self._rargs(input=r"echo -e '1,2,3,4,5,6'",
+        output, _ = self._rargs(input=r"echo -n '1,2,3,4,5,6'",
                                 args='-d ,',
-                                command='{} -e X{{7..}}X'.format(self._echo()))
-        self.assertEqual(output, "XX\n")
+                                command='echo -n X{7..}X')
+        self.assertEqual(output, "XX")
 
     def test_range_both(self):
-        output, _ = self._rargs(input=r"echo -e '1,2,3,4,5,6'",
+        output, _ = self._rargs(input=r"echo -n '1,2,3,4,5,6'",
                                 args='-d ,',
-                                command='{} -e "X{{3..3}}X"'.format(self._echo()))
-        self.assertEqual(output, "X3X\n")
+                                command='echo -n "X{3..3}X"')
+        self.assertEqual(output, "X3X")
 
-        output, _ = self._rargs(input=r"echo -e '1,2,3,4,5,6'",
+        output, _ = self._rargs(input=r"echo -n '1,2,3,4,5,6'",
                                 args='-d ,',
-                                command='{} -e "X{{3..4}}X"'.format(self._echo()))
-        self.assertEqual(output, "X3 4X\n")
+                                command='echo -n "X{3..4}X"')
+        self.assertEqual(output, "X3 4X")
 
-        output, _ = self._rargs(input=r"echo -e '1,2,3,4,5,6'",
+        output, _ = self._rargs(input=r"echo -n '1,2,3,4,5,6'",
                                 args='-d ,',
-                                command='{} -e "X{{3..7}}X"'.format(self._echo()))
-        self.assertEqual(output, "X3 4 5 6X\n")
+                                command='echo -n "X{3..7}X"')
+        self.assertEqual(output, "X3 4 5 6X")
 
-        output, _ = self._rargs(input=r"echo -e '1,2,3,4,5,6'",
+        output, _ = self._rargs(input=r"echo -n '1,2,3,4,5,6'",
                                 args='-d ,',
-                                command='{} -e "X{{4..3}}X"'.format(self._echo()))
-        self.assertEqual(output, "XX\n")
+                                command='echo -n "X{4..3}X"')
+        self.assertEqual(output, "XX")
 
     def test_field_separator(self):
-        output, _ = self._rargs(input=r"echo -e '1,2,3,4,5,6'",
+        output, _ = self._rargs(input=r"echo -n '1,2,3,4,5,6'",
                                 args='-d ,',
-                                command='{} -e "X{{3..4:_}}X"'.format(self._echo()))
-        self.assertEqual(output, "X3_4X\n")
+                                command='echo -n "X{3..4:_}X"')
+        self.assertEqual(output, "X3_4X")
 
-        output, _ = self._rargs(input=r"echo -e '1,2,3,4,5,6'",
+        output, _ = self._rargs(input=r"echo -n '1,2,3,4,5,6'",
                                 args='-d ,',
-                                command='{} -e "X{{3..4:-}}X"'.format(self._echo()))
-        self.assertEqual(output, "X3-4X\n")
+                                command='echo -n "X{3..4:-}X"')
+        self.assertEqual(output, "X3-4X")
 
     def test_global_field_separator(self):
-        output, _ = self._rargs(input=r"echo -e '1,2,3,4,5,6'",
+        output, _ = self._rargs(input=r"echo -n '1,2,3,4,5,6'",
                                 args='-d , -s /',
-                                command='{} -e "X{{3..4}}X"'.format(self._echo()))
-        self.assertEqual(output, "X3/4X\n")
+                                command='echo -n "X{3..4}X"')
+        self.assertEqual(output, "X3/4X")
 
-        output, _ = self._rargs(input=r"echo -e '1,2,3,4,5,6'",
+        output, _ = self._rargs(input=r"echo -n '1,2,3,4,5,6'",
                                 args='-d , -s /',
-                                command='{} -e "X{{3..4:,}}X"'.format(self._echo()))
-        self.assertEqual(output, "X3,4X\n")
+                                command='echo -n "X{3..4:,}X"')
+        self.assertEqual(output, "X3,4X")
 
 if __name__ == '__main__':
     unittest.main()
