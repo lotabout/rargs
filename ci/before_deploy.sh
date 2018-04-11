14ba6a2fdfcb7fd2900b2d4dbc46a2b889e1c9ca
Jinzhou Zhang <lotabout@gmail.com>
2018-04-11 08:11:48 +0800

    [ci] use trust instead of rust-everywhere

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -17,16 +17,15 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
-    cargo build --release --target $TARGET
+    # TODO Update this to build the artifacts that matter to you
+    cross build --target $TARGET --release
 
     # TODO Update this to package the right artifacts
     cp target/$TARGET/release/rargs $stage/
 
     cd $stage
-    tar czf $src/$PROJECT_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
     cd $src
 
     rm -rf $stage
 }
-
-main
